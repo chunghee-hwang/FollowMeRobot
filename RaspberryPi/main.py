2159d3aab580dee22eba8f160a82621e1c8f7850
hch0821 <hch0821@gmail.com>
2019-06-14 00:34:29 +0900

    Update main.py

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -8,42 +8,37 @@ from time import sleep
 from blueServer import *
 from multiprocessing import Process, Value
 
-def mainfunc(center, rssi, far_close):
+def mainfunc(center):
     # GPIO 모드 설정 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     try:
         while True:    
             #cls()
-            #print ("CENTER = %.1f cm" % center.value)
-            print ("rssi = %.1f dBm" % rssi.value)
-            print("far_close = %d" % far_close.value)
-
-            
+            print ("CENTER = %.1f cm" % center.value)
             #if center.value >= 50 and far_close.value == 0:
-            if far_close.value == 0:
-                setMotor(CH1, 45, FORWARD)
-                setMotor(CH2, 40, FORWARD)
+            #if far_close.value == 0:
+            #    setMotor(CH1, 45, FORWARD)
+            #    setMotor(CH2, 40, FORWARD)
             #elif center.value < 50 and far_close.value == 1:
-            else:
-                setMotor(CH1, 45, STOP)
-                setMotor(CH2, 45, STOP) 
-            sleep(0.5)
+            #else:
+            #    setMotor(CH1, 45, STOP)
+            #    setMotor(CH2, 45, STOP) 
+            #sleep(0.5)
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
 
 if __name__ == '__main__':
-    far_close = Value('d', -1.0)
-    rssi = Value('d', -1.0)
+
     center = Value('d', -1.0)
-    proc1 = Process(target=start_server, args=(far_close, rssi)) 
+    proc1 = Process(target=start_server) 
     proc3 = Process(target=distance, args=(TRIGGER_CENTER, ECHO_CENTER, center))
 
     proc1.start()
     proc3.start()
 
-    mainfunc(center, rssi, far_close)
+    mainfunc(center)
     proc1.join()
     proc3.join()
